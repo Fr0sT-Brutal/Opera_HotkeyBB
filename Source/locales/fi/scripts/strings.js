72a9eaa050f0422339f40accfded8f36ab8afc5b
Fr0sT <fr0st.brutal@gmail.com>
2012-10-19 16:18:17 +0400

    * Updated Finnish translation by Opeeera

--- Source/locales/fi/scripts/strings.js
+++ Source/locales/fi/scripts/strings.js
@@ -2,8 +2,9 @@
 
 var locStrings =
 {
-	VERSION: "1.5",
+	VERSION: "1.6",
 // Common pages
+	sHelpTranslating: "Auta kääntämisessä"
 	sFeedback: "Palaute",
 	sAnn: "Ilmoitukset (blogi)",
 	sChangelog: "Muutosloki",
@@ -11,29 +12,30 @@ var locStrings =
 	sPref: "Asetukset",
 	sWhatsTitle: "Mikä HotkeyBB?",
 	sWhatsPar1: "HotkeyBB on suunniteltu foorumeiden, blogien ja vieraskirjojen viestikenttiä varten. HotkeyBB:llä voit syöttää BB-kooditageja salamannopeasti riippumatta siitä, onko sivustolla valmiita painikkeita BB-koodia varten. Voit syöttää uuden BB-kooditagiparin tai ympäröidä valitun tekstin BB-kooditageilla. Laajennus tukee myös tageja, jotka tarvitsevat parametrin (kuten [tag=&quot;option&quot;]tekstiä[/tag]). Riippuen asetuksista, laajennus käyttää valittua tekstiä parametrina tai kysyy parametria sinulta.",
-	sWhatsPar2: 'HotkeyBB tukee myös sivustokohtaisia asetuksia, joiden avulla saat syötetyt tagit vastaamaan sivuston vaatimuksia. Saatavilla olevat asetukset ovat parametrien ympäröiminen lainausmerkeillä, sulkumerkki ( &lt; tai [ ) ja tagin merkkikoko.',
-	sWhatsPar3: "<strong>Varoitus!</strong> Laajennus pystyy sieppaamaan KAIKKI näppäinyhdistelmät, joten varo käyttämästä yleisiä yhdistelmiä (kuten Ctrl-Z, Ctrl-X, Ctrl-C, Ctrl-V ja Ctrl-A). Näppäinyhdistelmät tosin siepataan vain, kun tekstialue on kohdistettuna: muulloin yhdistemät toimivat normaalisti.",
+	sWhatsPar2: "HotkeyBB tukee myös sivustokohtaisia asetuksia, joiden avulla saat syötetyt tagit vastaamaan sivuston vaatimuksia. Saatavilla olevat asetukset ovat parametrien ympäröiminen lainausmerkeillä, sulkumerkki ( &lt; tai [ ) ja tagin merkkikoko.",
+	sWhatsPar3: "<strong>Varoitus!</strong> Laajennus pystyy sieppaamaan KAIKKI näppäinyhdistelmät, joten varo käyttämästä yleisiä yhdistelmiä (kuten Ctrl-Z, Ctrl-X, Ctrl-C, Ctrl-V ja Ctrl-A). Näppäinyhdistelmät tosin siepataan vain, kun tekstialue on kohdistettuna: muulloin yhdistelmät toimivat normaalisti.",
 	sSiteOptsTitle: "Sivustoasetukset",
 	sSiteOptsDescr: "Voit muuttaa tagiasetuksia vastaamaan foorumialustan vaatimuksia.",
-	sSiteOptsItem1: "Lainausmerkit — ympäröi parametrit lainausmerkeillä",
-	sSiteOptsItem2: "HTML-tagit — käytä HTML-tyylisiä tageja ( &lt;&gt; ) BB-koodin ( [] ) sijaan",
-	sSiteOptsItem3: "Isot kirjaimet — syötä tagit isoilla kirjaimilla",
+	sSiteOptsItem1: "<em>Lainausmerkit</em> — ympäröi parametrit lainausmerkeillä",
+	sSiteOptsItem2: "<em>HTML-tagit</em> — käytä HTML-tyylisiä tageja ( &lt;&gt; ) BB-koodin ( [] ) sijaan",
+	sSiteOptsItem3: "<em>Isot kirjaimet</em> — syötä tagit isoilla kirjaimilla",
 	sDefSiteOptsTitle: "Oletusasetukset",
 	sURLHdr: "URL-osoite",
 	sQuotesHdr: "Lainausmerkit",
 	sHTMLTagHdr: "HTML-tagit",
 	sTagUpCaseHdr: "Isot kirjaimet",
 	sSiteSpecTitle: "Sivustokohtaiset asetukset",
-	sSiteSpecDescr: "URL-osoite — sivuston osoite ilman protokollaprefixiä (\"http://\").<br/> <strong>Huom!</strong> Voit käyttää korvausmerkkejä alidomaineja varten, esimerkkejä: *.diary.ru — täsmää sivustoihin john.diary.ru ja mary.diary.ru; google.* — täsmää sivustoihin google.fi ja google.com; *.livejournal.* — täsmää sivustoihin john.livejournal.com ja diego.livejournal.es.<br/> <strong>Huom:</strong> Elleivät sivustokohtaiset asetukset toimi, voi olla, että tekstikenttä sijaitsee eri sivustossa kuin muu sisältö (näin on esimerkiksi blogspot.com-blogeissa). Siinä tapauksessa kohdista tekstikenttä ja paina debug-näppäinyhdistelmää <em>Ctrl-Vaihto-Alt-F1</em>.",
+	sSiteSpecDescr: "<em>URL-osoite</em> — sivuston osoite ilman protokollaprefixiä (\"http://\").<br/> <strong>Huom!</strong> Voit käyttää korvausmerkkejä alidomaineja varten, esimerkkejä: *.diary.ru — täsmää sivustoihin john.diary.ru ja mary.diary.ru; google.* — täsmää sivustoihin google.fi ja google.com; *.livejournal.* — täsmää sivustoihin john.livejournal.com ja diego.livejournal.es.<br/> <strong>Huom:</strong> Elleivät sivustokohtaiset asetukset toimi, voi olla, että tekstikenttä sijaitsee eri sivustossa kuin muu sisältö (näin on esimerkiksi blogspot.com-blogeissa). Siinä tapauksessa kohdista tekstikenttä ja paina debug-näppäinyhdistelmää <em>Ctrl-Vaihto-Alt-F1</em>.",
 	sDelOpt: "Poista",
 	sAddSiteOpt: "Lisää sivusto",
 	sTagListTitle: "Tagiluettelo",
 	sTagListDescr: "Tässä on luettelo käytettävissä olevista tageista. Voit vapaasti poistaa ja lisätä niitä.",
-	sTagListItem1: "Avaustagi — avaava tagi",
-	sTagListItem2: "Sulkutagi — sulkeva tagi; voi olla sama kuin avaava tagi, tyhjä (ts. [tag=&quot;option&quot;]) tai vapaavalintainen",
-	sTagListItem3: "Parametri — tagi tarvitsee parametrin (ts. [tag=&quot;option&quot;]tekstiä[/tag])",
-	sTagListItem4: "Valinta parametriksi — valittua tekstiä käytetään parametrina",
-	sTagListItem5: "Näppäinyhdistelmä — näppäinyhdistelmä, joka syöttää tagin",
+	sTagListItem1: "<em>Avaustagi</em> — avaava tagi",
+	sTagListItem2: "<em>Sulkutagi</em> — sulkeva tagi; voi olla sama kuin avaava tagi, tyhjä (ts. [tag=&quot;option&quot;]) tai vapaavalintainen",
+	sTagListItem3: "<em>Parametri</em> — tagi tarvitsee parametrin (ts. [tag=&quot;option&quot;]tekstiä[/tag])",
+	sTagListItem4: "<em>Valinta parametriksi</em> — valittua tekstiä käytetään parametrina",
+	sTagListItem5: "<em>Näppäinyhdistelmä</em> — näppäinyhdistelmä, joka syöttää tagin",
+	sHotkeyWarn: "<strong>Huom:</strong> Alt-näppäin -yhdistelmät eivät toimi, koska ne on varattu järjestelmän käyttöön (niillä aktivoidaan valikon toimintoja).<br/><strong>Huom:</strong> HotkeyBB ei välttämättä pysty sieppaamaan näppäinyhdistelmiä joillakin käyttöjärjestelmillä (GNU/Linux, ehkä myös Mac OS X), jos käytössä on esimerkiksi venäläinen tai kreikkalainen näppäimistö. Kyseessä on Operan bugi event.keyCode:n kanssa (korjattu versiossa 12.10).",
 	sOpenTag: "Avaustagi",
 	sCloseTag: "Sulkutagi",
 	sOption: "Parametri",
@@ -46,32 +48,36 @@ var locStrings =
 	sAddTag: "Lisää tagi",
 	sSettActions: "Toimenpiteet",
 	sExportSett: "Vie",
-	sExportSettDescr: "Tallenna HotkeyBB:n asetukset tekstitiedostoon",
+	sExportSettDescr: "Näytä HotkeyBB:n asetukset tekstimuodossa tallentamista varten",
 	sImportSett: "Tuo",
-	sImportSettDescr: "Lataa asetukset tiedostosta (ylikirjoittaa nykyiset asetukset)",
+	sImportSettDescr: "Lataa asetukset tekstistä/tiedostosta (<b>ylikirjoittaa</b> nykyiset asetukset)",
 	sMergeSett: "Yhdistä",
-	sMergeSettDescr: "Lataa asetukset tiedostosta ja yhdistä ne nykyisten asetusten kanssa",
+	sMergeSettDescr: "Lataa asetukset tekstistä/tiedostosta ja <b>yhdistä</b> ne nykyisten asetusten kanssa",
 	sExpTip: "Kopioi tämä teksti ja tallenna se millä tahansa tekstieditorilla (vaikkapa Muistiolla)",
 	sImpTip: "Liitä aiemmin tallennetut asetukset allaolevaan kenttään ja paina OK",
 	sImpAreUSure: "Tuonti ylikirjoittaa kaikki nykyiset asetukset! Haluatko jatkaa?",
 	sImpErr: "Asetuksia tuotaessa tapahtui virhe!",
-	sSavenClose: "Tallenna ja sulje",
+	sUseFileApi: "Tuo tiedostosta",
+	sFileApiDescr_File: "Asetukset tuodaan/yhdistetään tiedostosta. Poista valinta, jos haluat käyttää leikepöytää.",
+	sFileApiDescr_Clip: "Asetukset tuodaan/yhdistetään leikepöydältä. Poista valinta, jos haluat käyttää tiedostoa.",
+	sSaveNClose: "Tallenna ja sulje",
 // Changelog page
 	sVersHist: "Versiohistoria",
 	sVer: "Ver.",
 	sVerHilites: "Suuret muutokset:",
-	sVerChanges: "Kaikki muutokset:",
+	sVerChanges: "Pienet muutokset:",
 // Script messages
 	sEnterTag: "Syötä sulkeva tagi (ei saa olla tyhjä)",
+	sAltHotkeyWarn: "Syötit Alt-näppäin -yhdistelmiä seuraaville tageille:\n{tags}\nAlt-näppäin -yhdistelmät on varattu järjestelmän käyttöön, joten ne EIVÄT tule toimimaan!\nHaluatko korjata syöttämäsi näppäinyhdistelmät?",
 // Injected script messages
-	sEnterTagOption: "Syötä parametri <tag>-tagille.\nPaina \"Peruuta\", jos haluat jättää parametrin pois",
+	sEnterTagOption: "Syötä parametri {tag}-tagille.\nPaina \"Peruuta\", jos haluat jättää parametrin pois",
 	sDebugMsg: "HotkeyBB:n debug-tietoja.\n"+
-	           "Sivun osoite: <url>\n"+
-	           "Domain: <domain>\n"+
+	           "Sivun osoite: {url}\n"+
+	           "Domain: {domain}\n"+
 	           "Sivustokohtaiset asetukset:\n"+
-	           "   Lainausmerkit <quotes>\n"+
-	           "   HTML-tagit <tags>\n"+
-	           "   Isot kirjaimet <upcase>",
+	           "   Lainausmerkit {quotes}\n"+
+	           "   HTML-tagit {tags}\n"+
+	           "   Isot kirjaimet {upcase}",
 	sOn: "KÄYTÖSSÄ",
 	sOff: "POISSA KÄYTÖSTÄ"
 }
