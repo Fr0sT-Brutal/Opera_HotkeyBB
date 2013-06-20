c110da7e8db4b3c330833ebd915374ccfcd33440
Fr0sT-Brutal <fr0st.brutal@gmail.com>
2013-06-20 17:15:36 +0400

    * Build script tiny update

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 SET Name=HotkeyBB
-DEL Release\%Name%.oex 2> nul
-CD Source
+DEL Release\%Name%.oex 2> nul & ^
+CD Source & ^
 ..\7za.exe a -r -tzip -mx=9 ..\Release\%Name%.oex *
+if errorlevel 1 pause
